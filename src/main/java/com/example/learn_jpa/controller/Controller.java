1c985f38cd66c0b1369ee8c1e5835195fb532318
luck1306 <luck1306@naver.com>
2022-11-01 21:51:44 +0900

    add calling external api

--- src/main/java/com/example/learn_jpa/controller/Controller.java
+++ src/main/java/com/example/learn_jpa/controller/Controller.java
@@ -4,8 +4,12 @@ import com.example.learn_jpa.controller.dto.request.CreateMember;
 import com.example.learn_jpa.controller.dto.request.PostRequest;
 import com.example.learn_jpa.service.ControllerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -24,5 +28,10 @@ public class Controller {
     @PostMapping("/create-post")
     public void postMember(@RequestBody @Valid PostRequest postRequest) {
         controllerService.postCreate(postRequest);
+    }
+
+    @GetMapping("/data")
+    public ResponseEntity<Object> data(@RequestParam String uri) {
+        return controllerService.getData(uri);
     }
 }
--- src/main/java/com/example/learn_jpa/service/ControllerService.java
+++ src/main/java/com/example/learn_jpa/service/ControllerService.java
@@ -7,8 +7,21 @@ import com.example.learn_jpa.entity.member.repository.MemberRepository;
 import com.example.learn_jpa.entity.post.Post;
 import com.example.learn_jpa.entity.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ControllerService {
@@ -30,5 +43,39 @@ public class ControllerService {
                         .content(postRequest.getContent())
                         .member(member)
                 .build());
+    }
+
+    public ResponseEntity<Object> getData(String url) {
+        HashMap<String, Object> result = new HashMap<>();
+        ResponseEntity<Object> responseMap = new ResponseEntity<>(null, null, 200);
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            HttpHeaders headers = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+
+            responseMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Object.class);
+
+            result.put("statusCode", responseMap.getStatusCodeValue());
+            result.put("header", responseMap.getHeaders());
+            result.put("body", responseMap.getBody());
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getStatusCode());
+            result.put("body", e.getStatusText());
+            log.info("error");
+            log.info(e.toString());
+            return responseMap;
+        }
+        catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body", "exception error");
+            log.info(e.toString());
+
+            return responseMap;
+        }
+        System.out.println(responseMap);
+        return responseMap;
     }
 }
